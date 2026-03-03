c7e10bfd1d4fc0f5a106ff560041b904e5eb05b3
Jorge Francisco <jxfcx@uw.edu>
2026-03-03 11:16:36 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿/////////////////////////////////////////////////////////////////////////////////////
+// TINFO 200 B, Winter 2026
+// UWTacoma SET, Jorge Francisco-Chavez, Michael Caroll
+// 2026-03-02  - Program.cs
+//
+// Description - 
+//
+/////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////
+// Change History
+// Date ------- Developer -- Description
+// 2026-03-02 - Jorge -      
+//                           
+//
+// 2026-03-02 - Michael -    
+//                           
+//
+/////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpDB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create the database application instance and start the main menu loop.
+            DbApp db = new DbApp(); // There will only ever be one of these
+            //db.GoDatabase();
+        }
+    }
+
+}
